1b0da723c2fad3757868f4266a91286559dbe460
AmanBhaskar16 <amanbhaskar16@gmail.com>
2025-10-05 19:21:29 +0530

    changes in deployment

--- OrionEduverse_backend-main/src/index.js
+++ OrionEduverse_backend-main/src/index.js
@@ -30,7 +30,13 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 // Set up CORS (allow all origins by default, customize as needed)
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: "https://orion-ufot-git-main-amanbhaskar16s-projects.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 
 // Set up HTTP request logging
 app.use(morgan("dev"));
